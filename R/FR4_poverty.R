873c830a26cd6261cde3cf51bdd4eaf6dee42115
Martina Pardy <martinapardy@101-129.vpnc.wu-wien.ac.at>
2018-12-27 22:02:50 +0100

    #change in arop calculation

--- R/FR4_poverty.R
+++ R/FR4_poverty.R
@@ -6,71 +6,36 @@
 #3) Population living in very low intensity (quasijobless) households
 
 
-#1) At risk of poverty and social exclusion rate (AROPE) --------------------------------------------------
+#1) At risk of poverty rate (AROP) --------------------------------------------------
 
 #=percentage of people living in a household with an equivalised net disposable income below the at-risk- of-poverty threshold, set at 60% of the national median (after social transfers)
 
-#create datasets for different years
-silc.p105 <- silc.p1 %>% filter(rb010==2005)
-silc.p106 <- silc.p1 %>% filter(rb010==2006)
-silc.p107 <- silc.p1 %>% filter(rb010==2007)
-silc.p108 <- silc.p1 %>% filter(rb010==2008)
-silc.p109 <- silc.p1 %>% filter(rb010==2009)
-silc.p110 <- silc.p1 %>% filter(rb010==2010)
-silc.p112 <- silc.p1 %>% filter(rb010==2012)
-silc.p116 <- silc.p1 %>% filter(rb010==2016)
-
-
-#add median for each year
-silc.p105 <- silc.p105 %>% mutate(median=15878)
-silc.p116 <- silc.p116 %>% mutate(median=21993.85)
 
 #1) Use laeken package using y13
 library(laeken)
-arpr(inc = silc.p105$y13, weights = silc.p105$rb050) #=12.25
 
-arpr(inc = silc.p116$y13, weights = silc.p116$rb050) #=13.06
+arop <- arpr(silc.p1$y13, weights=silc.p1$rb050, years=silc.p1$rb010)
+arop
 
-#2) Manually computation using y13
-silc.p105 <- silc.p105 %>% mutate(x05=y13/median)
-nindivpoverty05 <- sum(silc.p105$x05<0.6)
-n05 <- nrow(silc.p105)
-nindivpoverty05/n05 #=12.94
 
-silc.p116 <- silc.p116 %>% mutate(x16=y13/median)
-nindivpoverty16 <- sum(silc.p116$x16<0.6)
-n16 <- nrow(silc.p116)
-nindivpoverty16/n16 #=12.57
+#2) Manual computation using y13
+#silc.p105 <- silc.p105 %>% mutate(x05=y13/median)
+#nindivpoverty05 <- sum(silc.p105$x05<0.6)
+#n05 <- nrow(silc.p105)
+#nindivpoverty05/n05 #=12.94
+
+#silc.p116 <- silc.p116 %>% mutate(x16=y13/median)
+#nindivpoverty16 <- sum(silc.p116$x16<0.6)
+#n16 <- nrow(silc.p116)
+#nindivpoverty16/n16 #=12.57
 
 #Check - use hy020
 #1) Use laeken package using hy020
-arpr(inc = silc.p105$hy020, weights = silc.p105$rb050) #=15.23
-arpr(inc = silc.p106$hy020, weights = silc.p106$rb050) #=15.88
-arpr(inc = silc.p107$hy020, weights = silc.p107$rb050) #=18.63
-arpr(inc = silc.p108$hy020, weights = silc.p108$rb050) #=17.74
-arpr(inc = silc.p110$hy020, weights = silc.p110$rb050) #=19.00
-arpr(inc = silc.p112$hy020, weights = silc.p112$rb050) #=19.43
-arpr(inc = silc.p116$hy020, weights = silc.p116$rb050) #=18.26 should be the right value, as same value as https://ec.europa.eu/eurostat/statistics-explained/index.php/People_at_risk_of_poverty_or_social_exclusion
+#arpr(inc = silc.p105$hy020, weights = silc.p105$rb050) =15.23
+#arpr(inc = silc.p116$hy020, weights = silc.p116$rb050) =18.26
 
-#2) Manually computation using hy020
-median.p1 <- svyby(~hy020, ~rb010, svy.p1, svyquantile, quantiles=0.5, keep.var = FALSE)
-median.p1
 
-#add median for each year
-silc.p105 <- silc.p105 %>% mutate(median=32242)
-silc.p116 <- silc.p116 %>% mutate(median=39760)
-
-silc.p105 <- silc.p105 %>% mutate(x2=hy020/median)
-sum(silc.p105$x2<0.6)
-nrow(silc.p105)
-2464/19020 #= 12.95
-
-silc.p116 <- silc.p116 %>% mutate(x2=hy020/median)
-sum(silc.p116$x2<0.6)
-nrow(silc.p116)
-4189/25617 #= 16.35
-
-#1a) GENDER: At risk of poverty and social exclusion rate ---------------------------------------------------
+#1a) GENDER: At risk of poverty rate ---------------------------------------------------
 
 #above 18
 
@@ -123,11 +88,11 @@ hs010 <- bind_rows(c06h, c07h, c08h, c09h, c10h, c11h, c12h, c13h, c14h, c15h, c
 silc.h <- left_join(silc.h, hs010 %>% select(hs011, hs010, hb010, hb030))
 rm(hs010, c07h, c08h, c09h, c10h, c11h, c12h, c13h, c14h, c15h, c16h, c17h)
 
-#datasets for 2005 & 2016
+#dataset for 2005 
 silc.h05 <- silc.h %>% filter(hb010==2005)
 silc.h00 <- silc.h05 %>%  dplyr::select(-c(hb010, hb030))
 
-#silc.h00 <- silc.h00 %>% select(c(hs110, hh050, hs040, hs050, hs060, hs070, hs080, hs100)) %>% add_count(n=2) 
+#silc.h00 <- dplyr::select(c(hs110, hh050, hs040, hs050, hs060, hs070, hs080, hs100)) %>% add_count() 
 
 #silc.h00 <- silc.h00 %>% count(c(hs110, hh050, hs040, hs050, hs060, hs070, hs080, hs100)==2) 
 
